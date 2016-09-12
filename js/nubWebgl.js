463787f9bd26c20e3b7664ae9b94c0db4a065833
samuelnub <notsamnub@gmail.com>
2016-09-12 18:04:28 +0100

    transparent???

--- js/nubWebgl.js
+++ js/nubWebgl.js
@@ -1,12 +1,12 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+var material = new THREE.MeshBasicMaterial({color: 0x6699ff});
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
@@ -15,6 +15,8 @@ camera.position.z = 5;
 var render = function()
 {
     requestAnimationFrame(render);
+
+    renderer.setClearColor(0x000000, 0);
     cube.rotation.x += 0.1;
     cube.rotation.y += 0.1;
     renderer.render(scene, camera);
