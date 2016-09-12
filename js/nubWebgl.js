11f2e1d43a0e2fd6056d1d3a78f41f1cbacaa05f
samuelnub <notsamnub@gmail.com>
2016-09-12 23:15:02 +0100

    more aesthetic?

--- js/nubWebgl.js
+++ js/nubWebgl.js
@@ -1,22 +1,25 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(16, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(8, window.innerWidth / window.innerHeight, 0.1, 1000);
 // Give it the alpha param, to allow a transparent canvas/background
 var renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.TorusKnotGeometry(1, 0.4, 128, 32);
-var material = new THREE.MeshLambertMaterial({color: 0x99ccff});
+var geometry = new THREE.TorusKnotGeometry(1, 0.32, 256, 16);
+var material = new THREE.MeshLambertMaterial({color: 0x80bfee, wireframe: false});
 var object = new THREE.Mesh(geometry, material);
+var object2 = new THREE.Mesh(geometry, material);
 
-var pointLight = new THREE.PointLight(0xffffff, 2, 32);
-var dirLight = new THREE.HemisphereLight(0xffffff, 0.6);
+var pointLight = new THREE.PointLight(0xffffff, 2, 2);
+var dirLight = new THREE.HemisphereLight(0xffffff, 0.8);
 pointLight.position.set(0,0,0);
+
 scene.add(object);
+scene.add(object2);
 scene.add(pointLight);
 scene.add(dirLight);
 
-camera.position.z = 16;
+camera.position.z = 30;
 
 var render = function()
 {
@@ -24,7 +27,11 @@ var render = function()
 
     renderer.setClearColor(0x000000, 0);
 
-    object.rotation.y += 0.01;
+    object.rotation.x += 0.005;
+    object.rotation.y += 0.0005;
+
+    object2.rotation.x += 0.0005;
+    object2.rotation.y += -0.005;
 
     renderer.render(scene, camera);
 };
