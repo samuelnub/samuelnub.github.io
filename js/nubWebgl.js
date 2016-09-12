cf9d788671dee1b7b87c8f26c1393b7e44873264
samuelnub <notsamnub@gmail.com>
2016-09-12 22:05:57 +0100

    a e s t h e t i c

--- js/nubWebgl.js
+++ js/nubWebgl.js
@@ -1,24 +1,31 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(16, window.innerWidth / window.innerHeight, 0.1, 1000);
 // Give it the alpha param, to allow a transparent canvas/background
 var renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({color: 0x99ccff});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var geometry = new THREE.TorusKnotGeometry(1, 0.4, 128, 32);
+var material = new THREE.MeshLambertMaterial({color: 0x99ccff});
+var object = new THREE.Mesh(geometry, material);
 
-camera.position.z = 5;
+var pointLight = new THREE.PointLight(0xffffff, 2, 32);
+var dirLight = new THREE.HemisphereLight(0xffffff, 0.6);
+pointLight.position.set(0,0,0);
+scene.add(object);
+scene.add(pointLight);
+scene.add(dirLight);
+
+camera.position.z = 16;
 
 var render = function()
 {
     requestAnimationFrame(render);
 
     renderer.setClearColor(0x000000, 0);
-    cube.rotation.x += 0.1;
-    cube.rotation.y += 0.1;
+
+    object.rotation.y += 0.01;
+
     renderer.render(scene, camera);
 };
 
